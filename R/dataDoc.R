5d6e6d2c5d7e71da1edc0929340501930b652de1
Hamere.M <hamereamogus@gmail.com>
2025-02-13 20:02:28 -0500

    Final package

---
+++ R/dataDoc.R
@@ -0,0 +1,10 @@
+#' MSfrags
+#' 
+#' some fragments with protein modifications that we might expect
+#' to find from the ccR5 gene in a mass spec experiment
+#' 
+#' 
+#' @format A character vector of length 4 with peptide sequence
+#' 
+#' @source data-raw/data.R "MSfrags"
+#' 
---
+++ tests/testthat/test-parse_mods.R
@@ -0,0 +1,8 @@
+test_that("parse_mods works", {
+  data(MSfrags)
+  
+  #check with one string
+  test1 <- parse_mods(MSfrags)
+  
+  expect_s3_class(test1, "protein_mods")
+})
